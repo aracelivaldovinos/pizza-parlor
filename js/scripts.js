c6f0da42b1c84debdc068fc03a50cb25df75078d
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-18 11:23:11 -0500

    add a totalOfToppings method

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,15 @@ function Pizza (plain, meatTopping,vegetableTopping, topping, size, cost) {
   this.cost = cost
 }
 
-
+Pizza.prototype.totalOfToppings = function () {
+  this.plain = 6
+  if (this.meatTopping === true && this,vegetableTopping === true) {
+  this.plain += this.topping
+  }
+  else if (this.meatTopping ===false) {
+    this.plain += this.topping
+  }
+  else {
+    this.plain += this.topping
+  }
+}
