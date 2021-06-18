cb323459764d12675016e86888e700a279ed8b1c
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-18 13:20:07 -0500

    WIP: add UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,52 @@
-let pizza1 = new Pizza (6,true,true,0,"large", 0)
 
-function Pizza (plain, meatTopping,vegetableTopping, topping, size, cost) {
+function Pizza (plain, topping, size, cost) {
   this.plain = plain
-  this.meatTopping = meatTopping
-  this.vegetableTopping = vegetableTopping
   this.topping = topping
   this.size = size
   this.cost = cost
 }
 
-Pizza.prototype.totalOfToppings = function () {
-  if (this.meatTopping === true && this,vegetableTopping === true) {
-  this.plain += this.topping
-  }
-  else if (this.meatTopping ===false) {
-    this.plain += this.topping
-  }
-  else {
-    this.plain += this.topping
-  }
-}
+// Pizza.prototype.totalOfToppings = function () {
+//   if (this.meatTopping === true && this,vegetableTopping === true) {
+//   this.plain += this.topping
+//   }
+//   else if (this.meatTopping ===false) {
+//     this.plain += this.topping
+//   }
+//   else {
+//     this.plain += this.topping
+//   }
+// }
 
-Pizza.prototype.sizeOfPizza = function() {
-  this.plain = 6
-  medium = 2
-  large = 4
-  if (this.size === "medium") {
-    this.plain += medium
-  }else if (this.size === "large") {
-    this.plain += large
-  }else {
-    this.plain = this.plain
-  }
-}
+// Pizza.prototype.sizeOfPizza = function() {
+//   this.plain = 6
+//   if (this.size === "medium") {
+//     this.plain += 2
+//   }else if (this.size === "large") {
+//     this.plain += 4
+//   }else {
+//     this.plain = this.plain
+//   }
+// }
 
 Pizza.prototype.totalCost = function() {
   this.cost += this.plain
 }
+
+let pizza1 = (0,0,0,0)
+
+$(document).ready(function(){
+    this.plain = 6
+  const size = $("select#size").val()
+  const meats = $("select#meats").val()
+  const veggies = $("select#veggies").val()
+  $("button#submit").click(function(event){
+    event.preventDefault();
+    if (size === $("#size").val("#small")) {
+      console.log(pizza1.plain)
+
+    }
+    
+    
+  })
+})
