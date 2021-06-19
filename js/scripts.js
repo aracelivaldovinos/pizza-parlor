04cd958674010003da3da1e699a423193a2306e0
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-18 23:55:25 -0500

    covert user topping to an array

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,6 @@ function Pizza (topping, size) {
 
 
 Pizza.prototype.pizzaSizeAmt = function () {
-  size = $("select#size").val()
   if (size === "small") {
     return 6
   }else if (size === "medium") {
@@ -17,7 +16,7 @@ Pizza.prototype.pizzaSizeAmt = function () {
 
 Pizza.prototype.toppingAmnt = function () {
   let toppingsPrice = 0
-  for (i =0; i<=this.topping.length; i++) {
+  for (let i =0; i<=this.topping.length; i++) {
     if (this.topping != "none") {
       let toppingPrice = this.topping.length * 2
       return toppingPrice
@@ -28,9 +27,31 @@ Pizza.prototype.toppingAmnt = function () {
 
 Pizza.prototype.totalCost = function(toppingAmnt, sizeAmnt) {
     let totalCost = toppingAmnt + sizeAmnt
-}
+  }
 
 
+
+
+// pizza = ([topping,topping], "size")
+//UI Logic
+$(document).ready(function(){
+  $("form#formOne").click(function(event){
+    event.preventDefault();
+    let meatArray = [];
+    $("input:checkbox[name=toppingCheck]:checked").each(function(){
+      let inputtedMeats = $(this).val()
+      meatArray.push(inputtedMeats);
+      $("#toppingsOutput").text(inputtedMeats + ",")
+    })
+    let size = $("select#size").val()
+    $("#sizeOutput").text(size)
+    
+
+
+  })
+  
+
+})
 
 
 
