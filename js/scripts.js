fddd5db959a5d59f10fc7b3af1c4305f8e72deef
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-20 00:50:35 -0500

    add input box for name and display on browser

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,10 @@ function Pizza (topping, size, cost) {
 
 Pizza.prototype.sizeAmnt = function () {
   sizePrice = 0
-  if (this.size === "small" ) {
+  if (this.size === "Small" ) {
     sizePrice = 6
   }
-  else if (this.size === "medium" ) {
+  else if (this.size === "Medium" ) {
     sizePrice = 8
   }
   else {
@@ -31,14 +31,18 @@ Pizza.prototype.totalCost = function (toppingPrice, sizePrice) {
 
 //UI Logic
 $(document).ready(function(){
-  $("button#add").click(function(){
+  $("button#add").click(function(event){
+    event.preventDefault();
     let toppingArray = $('input:checked').map(function(){
       return $(this).val();
     });
     $("#displayToppings").text(toppingArray.get())
     console.log(toppingArray.get());
 
-    size = $("select#size").val()
+    let name = $("input#name").val()
+    $("#displayName").text(name)
+
+    let size = $("select#size").val()
     $("#displaySize").text(size)
 
     let pizza = new Pizza (toppingArray, size)
