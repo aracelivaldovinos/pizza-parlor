70dcfb42c86ea41a6708ff411474d561acfa3ec1
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-19 23:16:26 -0500

    create a pizza constructor with properties

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function Pizza (topping, toppingPrice, size, sizePrice, cost) {
+  this.topping = topping
+  this.toppingPrice = toppingPrice
+  this.size = size
+  this.sizePrice = sizePrice
+  this.cost = cost
+}
